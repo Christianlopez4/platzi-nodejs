583293c124f7a762c589fa5c4bdb497be729bceb
christianlopez4 <christian.lopez4@correo.icesi.edu.co>
2021-01-18 17:35:55 -0500

    Cambio la configuración a la base de datos

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://db_user_platzi_nodejs:8wT0zb15JwsNsILl@cluster0.tyxbb.mongodb.net/test';
-
 mongoose.Promise = global.Promise;
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useUnifiedTopology", true);
 
-mongoose.connect(url);
-console.log('[db] Connected succesfully');
+async function connect(url) {
+    mongoose.set("useNewUrlParser", true);
+    mongoose.set("useUnifiedTopology", true);
+    await mongoose.connect(url);
+    console.log('[db] Connected succesfully');
+}
 
-module.exports = mongoose.connection;
+module.exports = connect;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const router = require('./network/routes');
 const bodyParser = require('body-parser');
 const db = require('./config/mongoose');
 
+const url = 'mongodb+srv://db_user_platzi_nodejs:8wT0zb15JwsNsILl@cluster0.tyxbb.mongodb.net/test';
+
+db(url);
 var app = express(); 
 app.use(bodyParser.json()); 
 router(app);
