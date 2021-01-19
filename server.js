4296b44f3aa3f91cc6dd736bff780d4f57459f80
christianlopez4 <christian.lopez4@correo.icesi.edu.co>
2021-01-18 22:57:11 -0500

    Elimino la URI

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const router = require('./network/routes');
 const bodyParser = require('body-parser');
 const db = require('./config/mongoose');
 
-const url = 'mongodb+srv://db_user_platzi_nodejs:8wT0zb15JwsNsILl@cluster0.tyxbb.mongodb.net/test';
+const url = '';
 
 db(url);
 var app = express(); 
